054c6b6dc70dd1cb4e7094a78e7628db9a84b801
Karol Kotański <karolfm12@onet.eu>
2024-01-09 14:24:09 +0100

    secondTaskBegin

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', function (time) {
+  console.log(time.seconds);
+});
