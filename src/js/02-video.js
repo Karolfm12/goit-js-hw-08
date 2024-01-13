af9c53a4459bf5eb3280718d8f7ead2f44987c22
Karolfm12 <karolfm12@onet.eu>
2024-01-13 10:17:14 +0100

    videoTask

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
@@ -11,6 +12,27 @@ player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
-player.on('timeupdate', function (time) {
-  console.log(time.seconds);
-});
+const setTime = throttle(function (time) {
+  const currentTime = time.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+  // console.log('current time:' + currentTime);
+}, 1000);
+player.on('timeupdate', setTime);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+// console.log('savedTime:' + savedTime);
+
+player
+  .setCurrentTime(savedTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
